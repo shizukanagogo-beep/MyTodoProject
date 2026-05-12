79b097c0566cccbe1158d3f4db8fd20e32c9aa64
shizukanagogo-beep <shizukana.gogo@gmail.com>
2026-05-12 19:15:10 +0900

    fix

--- AllTaskTodo/src/main/resources/schema.sql
+++ AllTaskTodo/src/main/resources/schema.sql
@@ -6,8 +6,8 @@ DROP TABLE if EXISTS categories;
 CREATE TABLE
     IF NOT EXISTS categories (
         id INT AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(50) NOT NULL
-        sort_order INT NULL;
+        name VARCHAR(50) NOT NULL,
+        sort_order INT NULL
     );
 
 ---------------------------------------------------------------------------
